047b5fef8eac7156633713b3562fd3cb8522d954
Pierre Anken <pierre.anken@pm.me>
2022-01-02 18:24:31 +0100

    context

--- Demo Projects/10 - side-effects-reducers-context/src/components/Login/Login.js
+++ Demo Projects/10 - side-effects-reducers-context/src/components/Login/Login.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
-import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -40,8 +39,6 @@ const Login = (props) => {
     value: '',
     isValid: null,
   });
-
-  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -90,7 +87,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
